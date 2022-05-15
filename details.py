e118706f7aa3b124c35bb8a094c238afbe3adc17
Proton03 <105384297+Proton03@users.noreply.github.com>
2022-05-15 15:09:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 18516364
-api_hash = "2e90970ea9e3ad55a3d7566754274fae"
-bot_token = "2057624136:AAGqDUgyUmtNZjeaO-ySc-YYJhTomhS1hmA"
+api_id = 9576601
+api_hash = "a9f1c1821757b62bc165ded384bad6a3"
+bot_token = "5367116295:AAGFnhzlW1ctnQqHIlOGSe2RcC9u8wdxh0M"
 
